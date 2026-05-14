b23008613c0483d1a2dcb726fb2756ca5c61de35
Jonathan <rochacontrerasjonathan40@gmail.com>
2026-05-14 12:10:45 -0400

    descomente la parte de mis rutas, createPost

--- lib/core/utils/app_routes.dart
+++ lib/core/utils/app_routes.dart
@@ -1,6 +1,10 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:mini_social_app/features/post/domain/usecases/create_post_usecase.dart';
+import 'package:mini_social_app/features/post/presentation/blocs/create_post_bloc.dart';
 import '../../features/post/presentation/pages/post_list_page.dart';
-
+import '../../features/post/presentation/pages/create_post_page.dart';
+import '../../features/post/data/repositories/mock_post_repository.dart';
 // Este archivo centraliza la navegación de la app siguiendo SRP.
 // Se ubica en lib/core/utils/ o lib/app_routes.dart (según escala).
 
@@ -13,11 +17,18 @@ final GoRouter router = GoRouter(
       builder: (context, state) => const PostListPage(),
     ),
     // Ejemplo de cómo se añadirían el resto:
-    // GoRoute(
-    //   path: '/create-post',
-    //   name: 'create_post',
-    //   builder: (context, state) => const CreatePostPage(),
-    // ),
+    GoRoute(
+      path: '/create-post',
+      name: 'create_post',
+      builder: (context, state) {
+        return BlocProvider(
+          create: (context) => CreatePostBloc(
+            createPostUseCase: CreatePostUseCase(MockRepository()),
+          ),
+          child: CreatePostPage(),
+        );
+      },
+    ),
     // GoRoute(
     //   path: '/favorites',
     //   name: 'favorites',
