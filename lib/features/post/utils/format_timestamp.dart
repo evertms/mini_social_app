a6288067ab85152f25e1dd420fcd9ac04a06d458
Rafael Vargas <123646379+rafael1199v@users.noreply.github.com>
2026-05-14 00:58:25 -0400

    feat: Util for timestamp formatting added

---
+++ lib/features/post/utils/format_timestamp.dart
@@ -0,0 +1,9 @@
+String formatTimestamp(DateTime timestamp) {
+    final day = timestamp.day.toString().padLeft(2, '0');
+    final month = timestamp.month.toString().padLeft(2, '0');
+    final year = timestamp.year.toString();
+    final hour = timestamp.hour.toString().padLeft(2, '0');
+    final minute = timestamp.minute.toString().padLeft(2, '0');
+
+    return '$day/$month/$year $hour:$minute';
+  }
